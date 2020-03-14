5fc1bcc03dcdedd8f421549b552b2f97d8f5ba9f
brds <brds>
2020-03-14 16:27:48 +0900

    Add a new file
    new py file

---
+++ sample.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from tqdm import tqdm
+import time
+
+def main():
+    for i in tqdm(range(100)):
+        time.sleep(0.02)
+
+if __name__ == '__main__': main()
+
+# test
+
+# branch2
